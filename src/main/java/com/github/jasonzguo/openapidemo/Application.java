c19ff5ba2e6a15751e0b4039493c6f58567438b7
Jason Guo <jiazheng.guo@outlook.com>
2019-08-18 22:00:17 -0400

    setup repo to generate code from open-api specs

---
+++ src/main/java/com/github/jasonzguo/openapidemo/Application.java
@@ -0,0 +1,11 @@
+package com.github.jasonzguo.openapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static  void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
